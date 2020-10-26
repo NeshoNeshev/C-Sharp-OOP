6b548d6e35b8b6e214b0189bc106969745a9f2f7
Nesho Neshev <neshev1978@gmail.com>
2020-10-26 21:35:11 +0200

    Add files via upload

---
+++ Inheritance/Need for Speed/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var car = new CrossMotorcycle(140,100);
+            car.Drive(40);
+            
+            System.Console.WriteLine(car.Fuel);
+        }
+    }
+}
