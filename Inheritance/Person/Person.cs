5b66c93db8003909b1ec32192c0581316967d15b
Nesho Neshev <neshev1978@gmail.com>
2020-10-26 19:27:36 +0200

    Add files via upload

---
+++ Inheritance/Person/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+        private int age;
+
+        public virtual int Age
+        {
+            get 
+            {
+                return this.age;
+            }
+            protected set
+            {
+                if (age >= 0)
+                {
+                    this.age = value;
+                }
+
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(String.Format("Name: {0}, Age: {1}",
+                         this.Name,
+                         this.Age));
+
+            return sb.ToString();
+        }
+
+    }
+}
