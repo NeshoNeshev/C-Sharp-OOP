d02598004544f63947e68a4ac928289f805b4370
Nesho Neshev <neshev1978@gmail.com>
2020-10-26 20:14:26 +0200

    Add files via upload

---
+++ Inheritance/Players and Monsters/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var monster = new SoulMaster("Ivan",12);
+            System.Console.WriteLine(monster);
+        }
+    }
+}
