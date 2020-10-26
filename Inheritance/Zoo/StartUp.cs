74d3485c308098d26e760466ca0bad6311f1c8e6
Nesho Neshev <neshev1978@gmail.com>
2020-10-26 19:45:38 +0200

    Add files via upload

---
+++ Inheritance/Zoo/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var snake = new Snake("pyton");
+            System.Console.WriteLine(snake);
+        }
+    }
+}
