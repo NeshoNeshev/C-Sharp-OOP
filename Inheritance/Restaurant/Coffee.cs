f40e86fee6e87482c31856e6a06b695ea4e29d6e
Nesho Neshev <neshev1978@gmail.com>
2020-10-27 17:55:30 +0200

    Add files via upload

---
+++ Inheritance/Restaurant/Coffee.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double coffeeMilliliters = 50;
+        private const decimal coffeePrice = 3.50m;
+        public Coffee(string name, double cafeine) : base(name, coffeePrice, coffeeMilliliters)
+        {
+            this.Caffeine = cafeine;
+        }
+        public double Caffeine { get; }
+    }
+}
