c3dc3120d9f4618978cbeb10e425a10b0235e779
Nesho Neshev <neshev1978@gmail.com>
2020-11-07 14:00:48 +0200

    Add files via upload

---
+++ Encapsulation/Class Box Data/Box.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace BoxData
+{
+    
+    public class Box
+    {
+        const int MIN_SIDE_VALUE = 0;
+        const int num = 2;
+        private double length;
+        private double width;
+        private double height;
+        private double result = 0;
+        public Box(double lenght, double width, double height)
+        {
+            this.Length = lenght;
+            this.Width = width;
+            this.Height = height;
+        }
+        public double Length
+        {
+            get => this.length;
+            set
+            {
+                if (value <= MIN_SIDE_VALUE)
+                {
+                    throw new Exception("Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            set
+            {
+                if (value <= MIN_SIDE_VALUE)
+                {
+                    throw new Exception("Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get => this.height;
+            set
+            {
+                if (value <= MIN_SIDE_VALUE)
+                {
+                    throw new Exception("Height cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+        public double LateralSurfaceArea()
+        {
+
+            result = num * (this.Length * this.Height) + num * (this.Width * this.Height);
+            return this.result;
+        }
+        public double SurfaceArea()
+        {
+            // 2lw + 2lh + 2wh
+            result = num * (this.length * this.width) + num * (this.length * this.height) + num * (this.width * this.height);
+            return result;
+        }
+        public double Volume()
+        {
+            //lwh
+            result = this.length * this.width * this.height;
+            return result;
+        }
+        
+    }
+}
---
+++ Encapsulation/Class Box Data/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BoxData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double l = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            
+            try
+            {
+              Box  box = new Box(l,w,h);
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+            }
+            catch (Exception ae)
+            {
+
+                Console.WriteLine(ae.Message);
+            }
+            
+        }
+    }
+}
